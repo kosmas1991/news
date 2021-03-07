103f7f09e6d51bdc5f006d3cba7e13fccce2738d
kosmas1991 <kosmasgou@gmail.com>
2021-03-07 11:04:05 +0200

    test implementation

--- lib/src/resources/news_api_provider.dart
+++ lib/src/resources/news_api_provider.dart
@@ -1,9 +1,10 @@
 import 'package:http/http.dart' show Client;
 import 'dart:convert';
 import '../models/item_model.dart';
+import 'repository.dart';
 
 final _root = 'https://hacker-news.firebaseio.com/v0';
-class NewsApiProvider{
+class NewsApiProvider implements Source{
   
   Client client = Client();
   
